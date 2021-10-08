4005e2ff9d81db7792760ceb3276d4f6ef38efc8
cobboldgit <augustinecobbold6@gmail.com>
2021-10-08 10:39:05 +0000

    create a function the recieves initialState  variable and action as it's parameter and return state

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,22 @@
+
+
+const initialState = {
+    users: [
+        {
+            name: 'Augustine',
+            phoneNumber: '0542222221',
+            location: 'Accra'
+        }, {
+            name: 'Augustine',
+            phoneNumber: '0542222221',
+            location: 'Accra'
+        },
+    ]
+}
+
+
+const contactReducer = ( state= initialState, action) => {
+    return state
+}
+
+export default contactReducer;
