648c183eee2d528dde41b174688c40f379b91a30
cobboldgit <augustinecobbold6@gmail.com>
2021-10-18 13:51:48 -0700

    clear initial contacts array in contactReducer

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,30 +1,18 @@
-
-
 const initialState = {
-    contacts: [
-        {
-            name: 'Augustine',
-            phoneNumber: '0542222221',
-            location: 'Accra'
-        }, {
-            name: 'Augustine',
-            phoneNumber: '0542222221',
-            location: 'Accra'
-        },
-    ]
-}
+  contacts: [],
+};
 
+const contactReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_CONTACT":
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
 
-const contactReducer = ( state= initialState, action) => {
-   switch (action.type) {
-       case 'ADD_CONTACT':
-           return {
-               ...state, contacts: [...state.contacts, action.payload]
-           }
-   
-       default:
-           return state
-   }
-}
+    default:
+      return state;
+  }
+};
 
 export default contactReducer;
