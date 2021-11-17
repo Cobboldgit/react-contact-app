1d35ed09f8b7ab8fa4053ec9257dde4d27a6c390
Augustine Cobbold <augustinecobbold6@gmail.com>
2021-11-17 07:50:35 +0000

    reduce and edit action for firestore

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -2,7 +2,10 @@ export const addContact = (contact) => {
   return (dispatch, state, { getFirestore }) => {
     getFirestore()
       .collection("contacts")
-      .add(contact)
+      .add({
+        ...contact,
+        timestamp: getFirestore().FieldValue.serverTimestamp(),
+      })
       .then(
         () => {},
         () => {}
@@ -15,37 +18,55 @@ export const addContact = (contact) => {
 };
 
 export const deleteContact = (contactId) => {
-  return {
-    type: "DELETE_CONTACT",
-    payload: contactId,
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("contacts")
+      .doc(contactId)
+      .delete()
+      .then(() => {});
   };
+  // return {
+  //   type: "DELETE_CONTACT",
+  //   payload: contactId,
+  // };
 };
 
 export const editContact = (newContact, contactId) => {
-  return {
-    type: "EDIT_CONTACT",
-    payload: {
-      newContact,
-      contactId,
-    },
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("contacts")
+      .doc(contactId)
+      .update(newContact)
+      .then(() => {});
   };
+  // return {
+  //   type: "EDIT_CONTACT",
+  //   payload: {
+  //     newContact,
+  //     contactId,
+  //   },
+  // };
 };
-
 
 export const getAllContacts = () => {
   return (dispatch, state, { getFirestore }) => {
     getFirestore()
       .collection("contacts")
-      .onSnapshot((querySnapshot) => {
-      let contact = [];
-        querySnapshot.forEach((doc) => contact.push(doc.data()));
-        dispatch({
-          type: "GET_ALL_CONTACTS",
-          payload: contact,
-        });
-      },
-      (e) => {
-        console.log(e);
-      });
+      .orderBy("timestamp", "desc")
+      .onSnapshot(
+        (querySnapshot) => {
+          let contact = [];
+          querySnapshot.forEach((doc) =>
+            contact.push({ ...doc.data(), id: doc.id })
+          );
+          dispatch({
+            type: "GET_ALL_CONTACTS",
+            payload: contact,
+          });
+        },
+        (e) => {
+          console.log(e);
+        }
+      );
   };
 };
